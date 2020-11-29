711fe0d2bd1a11403d333f9d80a80654dc45f2a9
lamnguyen5464 <lamnguyen5464@gmail.com>
2020-11-29 16:37:00 +0700

    add simple flow for app

--- src/Game.hpp
+++ src/Game.hpp
@@ -8,11 +8,18 @@ public:
     static const int SCREEN_HEIGHT = 700;
     static const int SCREEN_WIDTH = 1000;
 
+    static void clearConsole();
     static void goTo(int posX, int posY);
     static void setWindowSize(int width, int height);
     static bool controlDirectionKey(int &curX, int &curY, char signal);
     static bool haveStopSignal();
     static void addPixelToQueue(int x, int y, char pixel);
+    static int getColumns();
+    static int getRows();
+
+    //screens:
+    static void showMenu();
+    static void showGroundPlay();
 
     //in other thread
     static void drawPixelInQueue();
--- src/main.cpp
+++ src/main.cpp
@@ -2,68 +2,13 @@
 
 using namespace std;
 
-
-void testRun()
-{
-    int curX = 10, curY = 10;
-
-    char pixel = '\xDB';
-    vector<Coord> body;
-    body.push_back(Coord(0, 0));
-    body.push_back(Coord(1, 0));
-    body.push_back(Coord(0, 1));
-    body.push_back(Coord(1, 1));
-    body.push_back(Coord(2, 1));
-    body.push_back(Coord(-1, 1));
-    Truck obj(pixel, body);
-
-    obj.run();
-}
-
-void testPeople()
-{
-    int curX = 10, curY = 10;
-
-    char pixel = '#';
-    vector<Coord> body;
-    body.push_back(Coord(0, 0));
-    body.push_back(Coord(1, 1));
-    body.push_back(Coord(-1, 1));
-    body.push_back(Coord(0, 2));
-
-    Car car(pixel, body);
-
-    while (1)
-    {
-        if (Game().haveStopSignal()) return;
-        int oldX = curX, oldY = curY;
-        if (Game().controlDirectionKey(curX, curY, GlobalConfig::getInstance()->lastSignal)){
-            car.erase(oldX, oldY);
-            car.draw(curX, curY);
-            GlobalConfig::getInstance()->lastSignal = ' ';
-        }
-    }
-}
-
-
 int main()
 {
     Game().setWindowSize(Game().SCREEN_WIDTH, Game().SCREEN_HEIGHT);
     SetConsoleTitle("Game qua duong");
+    cout<<"Start test..."<<endl;
+    Game().showMenu();
 
-    cout << "Start test...." << endl;
-
-    thread one(testRun);
-    thread people(testPeople);
-    thread keyboardListener(Game().eventKeyBoardListener);
-    thread draw(Game().drawPixelInQueue);
-
-
-    keyboardListener.join();
-    draw.join();
-    people.join();
-
-    one.join();
-
+    Game().clearConsole();
     return 0;
 };
